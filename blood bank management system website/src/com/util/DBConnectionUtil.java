7c13ceb1f622f1f804f2972c5e16ddd9f4836aa8
BooleanAutocrad <100080205+BooleanAutocrad@users.noreply.github.com>
2022-08-17 11:32:51 +0530

    Add files via upload

---
+++ blood bank management system website/src/com/util/DBConnectionUtil.java
@@ -0,0 +1,25 @@
+package com.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnectionUtil {
+
+	
+	public static final String URL = "jdbc:mysql://localhost:3306/bbms?useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public static final String DRIVER = "com.mysql.jdbc.Driver";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "Chir@g2502";
+	public static Connection connection = null;
+
+	public static Connection openConnection() throws SQLException, ClassNotFoundException {
+		if (connection != null) {
+			return connection;
+		} else {
+			Class.forName(DRIVER);
+			System.out.println("loaded");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			System.out.println("connected");
+		}
+		return connection;
+	}
+}
